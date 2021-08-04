0592649cd19f8b6a8a2fb30274d0715fd7ae03c6
soongon <soongon@hucloud.co.kr>
2021-08-04 15:53:43 +0900

    initial commit

--- second_coupang_list.py
+++ second_coupang_list.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
@@ -26,6 +27,10 @@ for li_tag in li_tags:
     filename = image_url[image_url.rindex('/') + 1:]
     with open('./coupang_img/' + filename, 'wb') as f:
         f.write(img_res.content)
-        print(filename + ' 파일 쓰기완료')
+        # print(filename + ' 파일 쓰기완료')
 
-print(product_list)
+# 쿠팡 상품 데이터를 저장
+df = pd.DataFrame(product_list)
+df.columns = ['상품명', '가격', '좋아요', '이미지URL']
+df.to_csv('coupang.csv')
+df.to_excel('coupang.xlsx')
